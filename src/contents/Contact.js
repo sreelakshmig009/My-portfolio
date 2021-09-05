58cf098ce898cb4bfd7d2651f7e65f51faea3757
sreelakshmig009 <sreelakshmig009@gmail.com>
2021-09-06 01:39:58 +0530

    Developer's day week 1 task 2

---
+++ src/contents/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Social from '../components/Social';
+class Contact extends Component {
+    render() {
+        return (
+            <div className="condiv">
+            <h1 className="subtopic">Hit Me Up</h1>
+            <h3>Email : sreelakshmig009@gmail.com</h3>
+            <h3>Discord : G Sreelakshmi#5768</h3>
+            <Social />
+            </div>
+            )
+        }
+    }
+    
+    export default Contact;
+    
