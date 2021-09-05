6b9c25cb1e82031dcfd202e56bd9994aeb911720
sreelakshmig009 <sreelakshmig009@gmail.com>
2021-09-06 01:41:29 +0530

    Updates

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = () => (
-  <div className="footer">
-    <p>Big shoutout to Abdul Wahid Nafi whose medium blog helped me create this portfolio. Check out his medium profile <a href="https://medium.com/@iam_naafi">here.</a></p>
-  </div>
-);
-
-export default Footer;
---
+++ precache-manifest.22ffcea23be5253a78351b68f207b575.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e334f42bcda945c0f6cdb7c14889ef0c",
+    "url": "/My-portfolio/index.html"
+  },
+  {
+    "revision": "7e7340bcebc71260336f",
+    "url": "/My-portfolio/static/css/main.43999e80.chunk.css"
+  },
+  {
+    "revision": "ca3b5ccf1acb1c95684e",
+    "url": "/My-portfolio/static/js/2.785eb8e5.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/My-portfolio/static/js/2.785eb8e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e7340bcebc71260336f",
+    "url": "/My-portfolio/static/js/main.39719a78.chunk.js"
+  },
+  {
+    "revision": "177344f8239ba2199ba6",
+    "url": "/My-portfolio/static/js/runtime-main.34b95399.js"
+  },
+  {
+    "revision": "a1da13c1dcdc1f2a0967809534dcb88a",
+    "url": "/My-portfolio/static/media/pro-pic.a1da13c1.jpeg"
+  }
+]);
